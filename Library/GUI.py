a40056b75439bbd207baeabb9b43f3b5e260b234
hugnil <hugo.nilsson0002@gmail.com>
2024-07-29 15:17:00 +0200

    General structure and GUI

---
+++ Library/GUI.py
@@ -0,0 +1,46 @@
+import customtkinter as ctk
+from Logger import Logger
+from Login_page import Login_page
+from User import User
+from Main_lib import Main_lib
+
+
+class GUI:
+
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Library Management System")
+        self.root.geometry("400x300")
+        self.logger = Logger()
+        self.user = User()
+
+        self.create_pages()
+
+    def create_pages(self):
+        self.login_page = Login_page(self.root,
+                                     self.logger,
+                                     self.return_function,
+                                     self.user
+                                     )
+        self.main_lib = Main_lib(self.root,
+                                 self.logger,
+                                 self.return_function,
+                                 self.user
+                                 )
+        self.login_page.open()
+
+    def clear_frame(self, frame_to_clear):
+        frame_to_clear.clear()
+
+    def return_function(self, last_page, next_page, user):
+        exec(last_page)
+        exec(next_page)
+
+    def run_app():
+        root = ctk.CTk()
+        GUI(root)
+        root.mainloop()
+
+
+if __name__ == "__main__":
+    GUI.run_app()
---
+++ Library/Login_page.py
@@ -0,0 +1,81 @@
+import customtkinter as ctk
+
+
+class Login_page:
+    def __init__(self, root, logclass, return_function, user):
+        self.root = root
+        self.logclass = logclass
+        self.return_function = return_function
+        self.user = user
+
+        self.create_frames()
+
+    def create_frames(self):
+        self.login_frame = ctk.CTkFrame(
+            master=self.root,
+            fg_color='black'
+        )
+        self.login_frame.configure(
+            width=500,
+            height=300
+        )
+
+    def clear(self):
+        self.login_frame.pack_forget()
+
+    def open(self):
+        self.login_frame_open()
+
+    def login_frame_open(self):
+        self.login_frame.pack(
+            fill=ctk.BOTH,
+            expand=True
+        )
+        username_entry = ctk.CTkEntry(
+            master=self.login_frame,
+            fg_color='black',
+            bg_color='white',
+            font=('Arial', 12),
+            width=200,
+            height=30
+        )
+        username_entry.place(
+            x=150,
+            y=100,
+        )
+        password_entry = ctk.CTkEntry(
+            master=self.login_frame,
+            fg_color='black',
+            bg_color='white',
+            font=('Arial', 12),
+            width=200,
+            height=30
+        )
+        password_entry.place(
+            x=150,
+            y=150,
+        )
+        login_button = ctk.CTkButton(
+            master=self.login_frame,
+            text='Login',
+            command=lambda: self.login_button_clicked(
+                username_entry.get(),
+                password_entry.get()
+            ),
+            width=100,
+            height=30
+        )
+        login_button.place(
+            x=150,
+            y=200,
+        )
+
+    def login_button_clicked(self, username, password):
+        if username == 'admin' and password == 'admin':
+            self.logclass.log('Admin Login successful')
+            self.return_function('self.login_page.clear()',
+                                 'self.main_lib.open()',
+                                 self.user)
+        else:
+            self.logclass.log('Login failed')
+            self.root.destroy()
